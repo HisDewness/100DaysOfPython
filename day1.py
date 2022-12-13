3522d9b482558415e03988782be9a26added627a
Ismail Abbas <cherrius_macsimus@TheCherryTree.local>
2022-12-13 04:10:51 -0800

    Completed Interactive Exercises #1 & #2

--- day1.py
+++ day1.py
@@ -10,6 +10,14 @@ print("Hello world chief");
 hello_world = "Hello world"
 print(hello_world);
 
+# Printing a string literal across 3 lines but using 1 line of code
+print("Hello world!\nHello bum\nHello fam");
+
+# String concatenation
+print("Hello" + " " + "bruh")
+
+
+
 # Interactive Exercise #1 Problem
 # Write a program in main.py that prints the same 
 # notes from the previous lesson using what you have 
@@ -22,3 +30,16 @@ print(hello_world);
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+# Interactive Exercise #2 Problem: Debugging Practice
+# Look at the code in the code editor on the right. There 
+# are errors in all of the lines of code. Fix the code so 
+# that it runs without errors.
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the " '"+"'  + " sign.")
+print('e.g. print("Hello " + "world")');
+print("New lines can be created with a backslash and n.")
+
+
+
